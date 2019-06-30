1b053e655c2c2c7cd6325a124488f3674e896d36
Imo-Hetherington <44382444+Imo-Hetherington@users.noreply.github.com>
2019-06-30 19:58:27 +0100

    films column added

--- scripts.js
+++ scripts.js
@@ -9,40 +9,52 @@ container.setAttribute('class', 'container')
 
 const table = container.appendChild(document.createElement('table'));
 table.setAttribute('class', 'table')
+table.setAttribute('id', 'charTable')
 
 const headings = table.appendChild(document.createElement('tr'))
 table.setAttribute('scope', 'row')
 
 const nameheading = table.appendChild(document.createElement('th'))
 nameheading.textContent = 'Name'
-nameheading.setAttribute('scope', 'column')
 const heightheading = table.appendChild(document.createElement('th'))
 heightheading.textContent = 'Height'
 const massheading = table.appendChild(document.createElement('th'))
 massheading.textContent = 'Mass'
 const genderheading = table.appendChild(document.createElement('th'))
 genderheading.textContent = 'Gender'
+const filmheading = table.appendChild(document.createElement('th'))
+filmheading.textContent = 'Films'
 ;
 
 fetchData('https://swapi.co/api/people/').then((data) => {
-    data.results.forEach(result => {
-      //console.log(result.name)
+  for(var i = 0; i <   data.results.length; i++) {
+      var result = data.results[i]
       const row = document.createElement('tr')
       const name = document.createElement('td')
       row.appendChild(name)
       name.textContent = result.name
       const height = document.createElement('td')
       row.appendChild(height)
-      height.textContent = result.height
+      height.textContent = result.height + 'cm'
       const mass = document.createElement('td')
       row.appendChild(mass)
-      mass.textContent = result.mass
+      mass.textContent = result.mass +'kg'
       const gender = document.createElement('td')
       row.appendChild(gender)
       gender.textContent = result.gender
+      const films = document.createElement('td')
+      row.appendChild(films)
+      var episodelist = []
+      for (var j=0; j < result.films.length; j++) {
+        var episode = result.films[j]
+        episodelist.push(' ' + episode.substr(-2, 1))
+      }
+      films.textContent = 'Episodes: ' + episodelist
+
+      row.setAttribute('id', 'row-'+ (i+1))
 
       table.appendChild(row)
-    })
+    }
 })
 
 $('#searchbutton').click(function() {
@@ -56,13 +68,21 @@ $('#searchbutton').click(function() {
       name.textContent = result.name
       const height = document.createElement('td')
       row.appendChild(height)
-      height.textContent = result.height
+      height.textContent = result.height + 'cm'
       const mass = document.createElement('td')
       row.appendChild(mass)
-      mass.textContent = result.mass
+      mass.textContent = result.mass + 'kg'
       const gender = document.createElement('td')
       row.appendChild(gender)
       gender.textContent = result.gender
+      const films = document.createElement('td')
+      row.appendChild(films)
+      var episodelist = []
+      for (var j=0; j < result.films.length; j++) {
+        var episode = result.films[j]
+        episodelist.push(' ' + episode.substr(-2, 1))
+      }
+      films.textContent = 'Episodes: ' + episodelist
 
       table.appendChild(row)
       })
